d8cc65d8f19f74e9d887d8920e20d0149d4d99a2
jeorb <andy.meier@gmail.com>
2019-06-21 08:03:23 -0500

    Added contour parser

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::fmt;
 
 struct Map {
 
@@ -30,6 +31,16 @@ impl ByCode for PowerUp{
     }
 }
 
+struct Point {
+    x: usize,
+    y: usize,
+}
+impl fmt::Debug for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Point {{ x: {}, y: {} }}", self.x, self.y)
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -37,5 +48,25 @@ fn main() {
         .expect("Failed to read.");
 
     println!("{}: {}", filename, contents);
+
+    let parts:Vec<&str> = contents.split("#").collect();
+    let contour:Vec<&str> = parts[0]
+        .trim_matches('(')
+        .trim_matches(')')
+        .split("),(")
+        .collect();
+    let mut points:Vec<Point> = Vec::new();
+    for point in &contour {
+        let p:Vec<&str> = point.split(",").collect();
+        points.push(Point {
+            x: p[0].parse::<usize>().unwrap(),
+            y: p[1].parse::<usize>().unwrap(),
+        })
+    }
+
+    println!("contour: {:?}", contour);
+    println!("points: {:?}", points);
+
+
     println!("🌮 Free Tacos! 🌮");
 }
