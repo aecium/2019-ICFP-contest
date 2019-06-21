076429235eb6a54b0daefc52420bbec26cde5059
Nathan Frasier <nanoshot.nf@gmail.com>
2019-06-21 06:14:39 -0500

    impl some structs to make some stuff happen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+struct Map {
+
+}
+
+struct Bot {
+    powerups : Vec<PowerUp>
+}
+
+enum PowerUp{
+    Extension, //{code: 'B'},
+    Boost,// {code: 'F'},
+    Drill,// {code: 'L'},
+}
+ 
+pub trait ByCode {
+    fn by_code(code: char) -> Self;
+}
+
+impl ByCode for PowerUp{
+    fn by_code(code: char) -> Self {
+        match code {
+            'B' => PowerUp::Extension,
+            'F' => PowerUp::Boost,
+            'L' => PowerUp::Drill,
+            _ => panic!("Unknown powerup code"),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
