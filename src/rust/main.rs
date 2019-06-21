4e63f2970a9affff87cd96f47e29dd400dab038c
Nathan Frasier <nanoshot.nf@gmail.com>
2019-06-21 07:54:08 -0500

    WIP map parsing

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -2,11 +2,34 @@ use std::env;
 use std::fs;
 
 struct Map {
+    squares : Vec<Vec<MapSqare>>
+}
+impl Map {
+    fn from_map_string(map_string: &str) -> Self {
 
+        let split_map = map_string.split('#');
+        let bounding_box = split_map.next();
+        let bot_position = split_map.next();
+        let obstacles = split_map.next();
+        let boosters = split_map.next();
+
+        let map_vec = Vec::new();
+        for point_str in bounding_box.split(',')
+        {
+
+        }
+}
+
+enum MapSqare {
+    Empty { powerUp : Option<PowerUp>},
+    Wrapped { powerUp : Option<PowerUp>},
+    Blocked { powerUp : Option<PowerUp>},
+    OOB,
 }
 
 struct Bot {
     powerups : Vec<PowerUp>
+
 }
 
 enum PowerUp{
