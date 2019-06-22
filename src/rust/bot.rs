49d2708b3d3490d825143b7f48b7ee36fd0e84ac
aecium <aecium@gmail.com>
2019-06-21 22:24:19 -0500

    adding bot constructer and offsets

--- src/rust/bot.rs
+++ src/rust/bot.rs
@@ -3,7 +3,7 @@ use rand::{
     Rng,
 };
 
-use crate::app_core::{Direction, Point};
+use crate::app_core::{Direction, Point, Offset};
 
 pub struct Bot {
     pub Position: Point,
@@ -13,10 +13,21 @@ pub struct Bot {
     pub Drill: usize,
     //pub Mysterious_Point: usize,
     pub Teleports: usize,
-    pub Manipulators: Vec<Point>,
+    pub Manipulators: Vec<Offset>,
 }
 
 impl Bot {
+    pub fn new(tPosition: Point, tDirection: Direction)->Self{
+        Bot{Position: tPosition,
+        Facing: tDirection,
+        Extension: 0,
+        Boost: 0,
+        Drill: 0,
+        Teleports: 0,
+        Manipulators: vec![Offset{x:0,y:0},Offset{x:1,y:0},Offset{x:1,y:1},Offset{x:1,y:-1}], 
+        }
+    }
+
     pub fn move_self(&mut self, direction: &Direction) {
         let position = &self.Position;
         match direction {
